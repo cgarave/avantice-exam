f2cfc6e9ccf6b97faf658e7d7151d59dd09b451b
Rave Flores <reyvflores40@gmail.com>
2024-04-29 13:46:25 +0800

    Version 4

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 function googleTranslateElementInit() {
-    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+  new google.translate.TranslateElement(
+    { pageLanguage: "en" },
+    "google_translate_element"
+  );
 }
-//document.querySelector('.goog-te-combo').ariaLabel = 'Language';
-window.addEventListener('click', (e) => console.log(e.target))
+
+window.addEventListener("click", (e) => console.log(e.target));
+
+//Another solution to add webpage translation using Gtranslate API, use if hindi kailangan itranslate ang webpage sa lahat ng language
+
+// let x = document.getElementById("body");
+// document.getElementById("btn").onclick = translateLanguage;
+
+// async function translateLanguage() {
+//   const url = "https://google-translate1.p.rapidapi.com/language/translate/v2";
+//   const options = {
+//     method: "POST",
+//     headers: {
+//       "content-type": "application/x-www-form-urlencoded",
+//       "Accept-Encoding": "application/gzip",
+//       "X-RapidAPI-Key": "0e5b51a8c9msh12406982188927bp1a171ajsnffc740228f82",
+//       "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+//     },
+//     body: new URLSearchParams({
+//       q: x.innerHTML,
+//       target: "ko", //translate to korean
+//       source: "en",
+//     }),
+//   };
+
+//   try {
+//     const response = await fetch(url, options);
+//     const result = await response.json();
+//     console.log(result);
+//     x.textContent = result.data.translations[0].translatedText;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
